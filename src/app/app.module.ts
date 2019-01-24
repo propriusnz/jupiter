8f78add3276885d673da1389981da5e901686e71
Angela <xoneonxero@gmail.com>
2019-01-24 14:25:47 +1300

    add faq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './fcomponents/basic/navbar/navbar.component';
 import {HomepageComponent} from './fcomponents/pages/homepage/homepage.component';
+import { FaqComponent } from './fcomponents/pages/faq/faq.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomepageComponent
+    HomepageComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
