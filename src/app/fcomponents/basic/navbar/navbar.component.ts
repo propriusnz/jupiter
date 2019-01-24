3100c53a73fbc5c968c3d1aa9459ac95368b1752
lgxkevin <lgx9587@gmail.com>
2019-01-24 22:37:26 +1300

    navbar/faq/style.css

--- src/app/fcomponents/basic/navbar/navbar.component.ts
+++ src/app/fcomponents/basic/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
 export class NavbarComponent implements OnInit {
-
+  isTop:boolean = true
   constructor() { }
-
   ngOnInit() {
-  }
 
+  }
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event){
+    let positionNav:number = window.pageYOffset
+    if (positionNav <= 70){
+      this.isTop = true;
+    }else{
+      this.isTop = false;
+
+    }
+  } 
 }
