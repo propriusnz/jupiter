125710503f172a16e979af8831eb701c6d321a42
lgxkevin <lgx9587@gmail.com>
2019-01-30 15:46:06 +1300

    chooseQuantity

--- src/app/fcomponents/products/product/product.component.ts
+++ src/app/fcomponents/products/product/product.component.ts
@@ -11,6 +11,10 @@ import { error } from '@angular/compiler/src/util';
 export class ProductComponent implements OnInit {
   productId:number
   productDetail:any;
+  quantity:number=1 ;
+  quantityLength:number;
+  quantityError:boolean = false
+  quantityFilled:boolean = true
   constructor(
     private route:ActivatedRoute,
     private productService:ProductService
@@ -23,10 +27,24 @@ export class ProductComponent implements OnInit {
     this.productService.showProduct(this.productId).subscribe( 
       (res)=>{
         this.productDetail = res['Data']
+        this.quantityLength = this.productDetail.TotalStock.toString().length;
         console.log(this.productDetail)
       },
       (error)=>console.log(error)
     )
   }
-
+  quanCheck(e){
+    if(e){
+      this.quantityFilled = true
+      if(e>this.productDetail.TotalStock){
+        this.quantity = this.productDetail.TotalStock
+      }
+      if(e.toString().slice(0,1)=='-'){
+        this.quantity = 1
+      }
+    }
+    if(!e){
+      this.quantityFilled = false
+    }
+  }
 }
