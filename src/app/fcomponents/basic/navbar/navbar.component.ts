e046fb1157b8fb339ed6ea8dfba8d8d8e6bd117b
Andrew-Yan91 <andrewyan91@hotmail.com>
2019-01-31 15:57:32 +1300

    adjusted layout

--- src/app/fcomponents/basic/navbar/navbar.component.ts
+++ src/app/fcomponents/basic/navbar/navbar.component.ts
@@ -11,26 +11,28 @@ export class NavbarComponent implements OnInit {
   constructor() { }
   ngOnInit() {
     
-    // window.onscroll=function(){
-    //   var h =document.documentElement.scrollTop||document.body.scrollTop;
-    //   console.log(h*2.5);      //控制台查看监听滚动  
-    //   var headerTop =document.getElementById("navbarId");            
-    //   if( h >=40) {       //header的高度是40px;     
-    //     headerTop.style.background="#fff";            
-    //     headerTop.style.color="rgba(66,65,66,1)";            
-    //   }else{    
-    //     if(h<10){
-    //               //40*2.5=100;这样透明度就可以由0渐变到100%；
-    //       headerTop.style.background="rgba(255,255,255,0.0"+h*2.5+")";  
-    //       headerTop.style.color="rgba(66,66,66,0.0"+h*2.5+")";            
-    //     }else if(h>10 && h<= 40){
-    //       headerTop.style.background="rgba(255,255,255,0."+h*2.5+")"; 
-    //       headerTop.style.color="rgba(66,66,66,0."+h*2.5+")";            
+    window.onscroll=function(){
+      var h =document.documentElement.scrollTop||document.body.scrollTop;
+      console.log(h*1.4);      
+      var headerTop =document.getElementById("navbarId");            
+      if( h >=70) {       //header.height= 70px;     
+        headerTop.style.background="#fff";            
+        headerTop.style.color="rgba(66,65,66,1)";            
+      }else{    
+        if(h<30){
+          headerTop.style.background="rgba(255,255,255,0.0"+h*1.4+")";  
+          headerTop.style.color="rgba(66,66,66,0.0"+h*1.4+")";  
+          headerTop.style.height="130px";          
+        }else if(h>30 && h<= 70){
+          headerTop.style.background="rgba(255,255,255,0."+h*1.4+")"; 
+          headerTop.style.color="rgba(66,66,66,0."+h*1.4+")";
+          headerTop.style.height="60px";
+          headerTop.style.transitionDuration="1s";  
    
-    //     }            
+        }            
    
-    //   }        
-    // };
+      }        
+    };
   }
   
   @HostListener('window:scroll', ['$event']) onScrollEvent($event){
