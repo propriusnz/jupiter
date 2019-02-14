64afea3ced4cacf03f449cc9e95efc7614cb31ed
lgxkevin <lgx9587@gmail.com>
2019-02-14 13:03:37 +1300

    HomepageUpdate

--- src/app/fcomponents/pages/homepage/homepage.component.ts
+++ src/app/fcomponents/pages/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef, ViewChild,HostListener} from '@angular/core';
 interface productList {
   productName:string;
   imgLink:string;
@@ -15,9 +15,21 @@ export class HomepageComponent implements OnInit {
     {productName: "product3",imgLink:"https://www.thebigletterco.com.au/wp-content/uploads/2013/10/3.jpg"},
     {productName: "product4",imgLink:"https://www.thebigletterco.com.au/wp-content/uploads/2013/10/4.jpg"}
   ]
+  @ViewChild ('bgat') bgat:ElementRef
   constructor() { }
 
   ngOnInit() {
-  }
+    window.onscroll = ()=>{
+      let top1 = this.bgat.nativeElement.offsetTop;
+      if (top1 - window.pageYOffset < window.innerHeight && top1>window.pageYOffset){
+        console.log("Reached bgat");
+        this.backgroundscroll(top1 - window.pageYOffset)
+      }
 
+    }
+  }
+  backgroundscroll(e){
+    console.log(e)
+    this.bgat.nativeElement.style.backgroundPosition = "0%"+ e/3 +"%"
+  }
 }
