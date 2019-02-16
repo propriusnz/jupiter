eeaf568af3736f4bc83247447bade42eeb3343a0
lgxkevin <lgx9587@gmail.com>
2019-02-16 23:43:49 +1300

    footerChanges

--- src/app/fcomponents/pages/homepage/homepage.component.ts
+++ src/app/fcomponents/pages/homepage/homepage.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit,ElementRef, ViewChild,HostListener} from '@angular/core';
-interface productList {
-  productName:string;
-  imgLink:string;
-}
+import { Component, OnInit,ElementRef, ViewChild} from '@angular/core';
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  products:productList[] = [
-    {productName: "product1", imgLink:"https://www.thebigletterco.com.au/wp-content/uploads/2013/10/1.jpg"},
-    {productName: "product2", imgLink:"https://www.thebigletterco.com.au/wp-content/uploads/2013/10/2.jpg"},
-    {productName: "product3",imgLink:"https://www.thebigletterco.com.au/wp-content/uploads/2013/10/3.jpg"},
-    {productName: "product4",imgLink:"https://www.thebigletterco.com.au/wp-content/uploads/2013/10/4.jpg"}
-  ]
+
   @ViewChild ('bgat') bgat:ElementRef
   constructor() { }
 
