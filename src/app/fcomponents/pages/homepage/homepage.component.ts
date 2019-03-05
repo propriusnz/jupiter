3b580261cc58aea26ca159d6f64e04a0cf065e12
lgxkevin <lgx9587@gmail.com>
2019-03-05 16:07:09 +1300

    changes1

--- src/app/fcomponents/pages/homepage/homepage.component.ts
+++ src/app/fcomponents/pages/homepage/homepage.component.ts
@@ -7,19 +7,35 @@ import { Component, OnInit,ElementRef, ViewChild} from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  num:number = 0;
   @ViewChild ('bgat') bgat:ElementRef
+  @ViewChild ('imgScroll') imgScroll:ElementRef
+  @ViewChild ('list') list:ElementRef
+
   constructor() { }
 
   ngOnInit() {
+    //large screen
+    console.log(window);
+    this.imgScroll.nativeElement.style.height = window.innerHeight - 190 +"px";
+    //mobile screen
+    if (window.innerWidth<768){
+      this.imgScroll.nativeElement.style.height = window.innerHeight - 100 +"px";
+    }
+    console.log(this.imgScroll);
+    
+
     window.onscroll = ()=>{
       let top1 = this.bgat.nativeElement.offsetTop;
       if (top1 - window.pageYOffset < window.innerHeight && top1+this.bgat.nativeElement.offsetHeight>window.pageYOffset){
         this.backgroundscroll(top1 - window.pageYOffset)
       }
-
     }
+  }
+  ngAfterViewInit(): void {
   }
   backgroundscroll(e){
     this.bgat.nativeElement.style.backgroundPosition = "0%"+ e/3 +"%"
   }
+
 }
