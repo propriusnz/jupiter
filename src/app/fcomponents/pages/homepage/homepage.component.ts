2c9944851135648ee065f71041f94d27508d0499
qmqd1225 <qmqd1225@gmail.com>
2019-03-21 14:44:29 +1300

    img update

--- src/app/fcomponents/pages/homepage/homepage.component.ts
+++ src/app/fcomponents/pages/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,7 +7,7 @@ import { Component, OnInit,ElementRef, ViewChild} from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  num:number = 0;
+  num: number = 0;
   @ViewChild ('bgat') bgat:ElementRef
   @ViewChild ('imgScroll') imgScroll:ElementRef
   @ViewChild ('list') list:ElementRef
@@ -15,28 +15,29 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    //large screen
+    // large screen
     console.log(window);
-    this.imgScroll.nativeElement.style.height = window.innerHeight - 190 +"px";
-    //mobile screen
-    if (window.innerWidth<768){
-      this.imgScroll.nativeElement.style.height = window.innerHeight - 100 +"px";
+    this.imgScroll.nativeElement.style.height = window.innerHeight - 190 + 'px';
+    // mobile screen
+    if (window.innerWidth < 768) {
+      this.imgScroll.nativeElement.style.height = window.innerHeight - 100 + 'px';
     }
     console.log(this.imgScroll);
-    
 
-    window.onscroll = ()=>{
+
+    window.onscroll = () => {
       let top1 = this.bgat.nativeElement.offsetTop;
-      if (top1 - window.pageYOffset < window.innerHeight && top1+this.bgat.nativeElement.offsetHeight>window.pageYOffset){
+      if (top1 - window.pageYOffset < window.innerHeight && top1 + this.bgat.nativeElement.offsetHeight > window.pageYOffset) {
         this.backgroundscroll(window.pageYOffset);
       }
-    }
+    };
   }
+  // tslint:disable-next-line:use-life-cycle-interface
   ngAfterViewInit(): void {
   }
   // controll scrolling speed
-  backgroundscroll(e){
-    this.bgat.nativeElement.style.backgroundPosition = "0%"+ e/50 +"%"
+  backgroundscroll(e) {
+    this.bgat.nativeElement.style.backgroundPosition = '0%' + e / 50 + '%';
   }
 
 }
