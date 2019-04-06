ee25e0d621284610819c750b8a5102b08b431533
lgxkevin <lgx9587@gmail.com>
2019-04-06 23:54:24 +1300

    css update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  //baseUrl: 'http://luxedreameventhire.co.nz:5000/api',
-  baseUrl: 'http://localhost:5000/Api',
-=======
   baseUrl: 'http://luxedreameventhire.co.nz:5000/api',
-  // baseUrl: 'https://localhost:44364/Api',
->>>>>>> 34327be3213e1148ea37a9fec2c3f11de8d6af97
+  //baseUrl: 'https://localhost:5000/Api',
 };
