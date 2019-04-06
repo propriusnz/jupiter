6c73f88fe3321b1ca85053554396d7fdafc04e7e
Mike <info@learnspace.co.nz>
2019-04-06 18:19:32 +1300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   //TODO: change baseUrl
   production: true,
-<<<<<<< HEAD
   baseUrl: 'http://luxedreameventhire.co.nz:5000/api',
   // baseUrl: 'https://localhost:44364/Api',
-=======
-  //baseUrl: 'http://luxedreameventhire.co.nz:5000/api',
-  baseUrl: 'http://localhost:5000/Api',
->>>>>>> 6d059bf0610372e794d588154c4742556e5bd91f
 };
