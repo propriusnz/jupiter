3d322a98afc2df6bc6192b995e06be8a78889681
lgxkevin <lgx9587@gmail.com>
2019-04-16 13:32:41 +1200

    update

--- src/app/fcomponents/products/product/product.component.ts
+++ src/app/fcomponents/products/product/product.component.ts
@@ -8,10 +8,10 @@ import { Validators, FormBuilder, FormGroup, FormArray } from '@angular/forms';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  productId:number
-  productDetail:any;
-  prodMediaUrl:any;
-  quantity:number=1 ;
+  productId: number;
+  productDetail: any;
+  prodMediaUrl: any;
+  quantity = 1 ;
   isprodAdded:boolean = false;
   isStockAvailable:boolean = true;
   cartList = [];
@@ -29,8 +29,8 @@ export class ProductComponent implements OnInit {
   ngOnInit() {
     this.cartForm = this.formBuilder.group({
       cartItems: this.formBuilder.array([])
-    });    
-    //get the detail of product
+    });
+    // get the detail of product
     this.productService.showProduct(this.productId).subscribe( 
       (res)=>{
         this.productDetail = res
@@ -43,17 +43,17 @@ export class ProductComponent implements OnInit {
     this.setStorage()
     }
 
-  //add product descriptions into formArray
+  // add product descriptions into formArray
   createItem(res){
     res['productDetail'].forEach(prod => {
       let control = <FormArray>this.cartForm.controls.cartItems
       control.push(
         this.formBuilder.group({
-          ProdId:prod.prodId,
-          Title:prod.productDetail1,
-          Quantity:[0,[Validators.min(0), Validators.max(prod.availableStock)]],
-          Price:(prod.discount && prod.discount>0) ? prod.price - prod.discount: prod.price,
-          Discount:prod.discount,
+          ProdId: prod.prodId,
+          Title: prod.productDetail1,
+          Quantity: [0,[Validators.min(0), Validators.max(prod.availableStock)]],
+          Price: (prod.discount && prod.discount>0) ? prod.price - prod.discount: prod.price,
+          Discount: prod.discount,
           AvailableStock: prod.availableStock
         })
       )
@@ -61,28 +61,28 @@ export class ProductComponent implements OnInit {
 
   } 
 
-  //get cartlist from localStorage 
+  // get cartlist from localStorage 
   setStorage(){
       if ('cartList' in localStorage){
         this.cartList = JSON.parse(localStorage.getItem('cartList'))
       }else{
-        localStorage.setItem('cartList',JSON.stringify(this.cartList))
+        localStorage.setItem('cartList', JSON.stringify(this.cartList))
       }
-      localStorage.setItem('userId','aaa ')
+      localStorage.setItem('userId', 'aaa ')
   }
-  //add product into a list
+  // add product into a list
   manageCartProds(){
     let newCartList = []
       // if product detail exist
-    if(this.productDetail['productDetail'] && this.productDetail['productDetail'].length!=0){
+    if(this.productDetail['productDetail'] && this.productDetail['productDetail'].length !== 0){
       this.cartForm.controls.cartItems['value'].forEach(cartItem => {
         let item = {
-          ProdId:cartItem.ProdId,
-          Price: cartItem.Price*cartItem.Quantity,
-          Title: this.productDetail.title + ': ' + cartItem.Title, 
-          Quantity:cartItem.Quantity
+          ProdId: cartItem.ProdId,
+          Price: cartItem.Price * cartItem.Quantity,
+          Title: this.productDetail.title + ': ' + cartItem.Title,
+          Quantity: cartItem.Quantity
         }
-        //add product into cart only if amount > 0
+        // add product into cart only if amount > 0
         if(item.Quantity != 0){
           newCartList.push(item)
         }
@@ -97,7 +97,7 @@ export class ProductComponent implements OnInit {
         Title:this.productDetail.title,
         Quantity:this.quantity
       }
-      //Quantity check
+      // Quantity check
       if(this.quantity <= this.productDetail.availableStock){
         newCartList.push(item)
         this.addToCart(newCartList)
