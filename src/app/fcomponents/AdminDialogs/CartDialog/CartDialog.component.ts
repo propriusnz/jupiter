ebadce58f77062bf2d7aa3f596343d940a4ed5d4
lgxkevin <lgx9587@gmail.com>
2019-04-30 22:01:21 +1200

    bug fix

--- src/app/fcomponents/AdminDialogs/CartDialog/CartDialog.component.ts
+++ src/app/fcomponents/AdminDialogs/CartDialog/CartDialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject, EventEmitter } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
-import { ProductService } from '../../../service/product.service'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { ProductService } from '../../../service/product.service';
 
 @Component({
   selector: 'app-CartDialog',
@@ -8,95 +8,95 @@ import { ProductService } from '../../../service/product.service'
   styleUrls: ['./CartDialog.component.css']
 })
 export class CartDialogComponent implements OnInit {
-  cartId:number;  
-  contactId:number;
-  status:string
-  displayData:any
-  title:string
-  cartProdList:any
+  cartId: number;
+  contactId: number;
+  status: string;
+  displayData: any;
+  title: string;
+  cartProdList: any;
   dataChanges = new EventEmitter();
-  isLoading:boolean = false
-  isCardProdDeleted:boolean = false
+  isLoading = false;
+  isCardProdDeleted = false;
   contactForm = {
-    email:'',
-    firstName:'',
-    lastName:'',
-    message:'',
-    phoneNum:''
-  }
+    email: '',
+    firstName: '',
+    lastName: '',
+    message: '',
+    phoneNum: ''
+  };
   cartForm = {
-    location:'',
-    plannedTime:'',
-    price:0,
-  }
+    location: '',
+    plannedTime: '',
+    price: 0,
+  };
   constructor(
     private dialogRef: MatDialogRef<CartDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private productService : ProductService
-  ) { 
+    private productService: ProductService
+  ) {
     // fill in contact form
-    this.contactForm.email = data.data['contact']['email']
-    this.contactForm.firstName = data.data['contact']['firstName']
-    this.contactForm.lastName = data.data['contact']['lastName']
-    this.contactForm.phoneNum = data.data['contact']['phoneNum']
-    this.contactForm.message = data.data['contact']['message']
+    this.contactForm.email = data.data['contact']['email'];
+    this.contactForm.firstName = data.data['contact']['firstName'];
+    this.contactForm.lastName = data.data['contact']['lastName'];
+    this.contactForm.phoneNum = data.data['contact']['phoneNum'];
+    this.contactForm.message = data.data['contact']['message'];
     // fill in cart form
-    this.cartForm.location = data.data['location']
-    this.cartForm.plannedTime = data.data['plannedTime']
-    this.cartForm.price  = data.data['price']
+    this.cartForm.location = data.data['location'];
+    this.cartForm.plannedTime = data.data['plannedTime'];
+    this.cartForm.price  = data.data['price'];
 
-    this.cartId = data.data['id']
-    this.displayData = data.data
-    this.title = data.title
-    this.status = data.action
+    this.cartId = data.data['id'];
+    this.displayData = data.data;
+    this.title = data.title;
+    this.status = data.action;
   }
 
   ngOnInit() {
-    this.getCartProds()
+    this.getCartProds();
   }
   close() {
     this.dialogRef.close();
   }
-  updateContact(){
-    this.isLoading = true
+  updateContact() {
+    this.isLoading = true;
     this.productService.updateContacts(this.displayData['contactId'], this.contactForm).subscribe(
-      (res)=>{
-        this.isLoading = false
+      (res) => {
+        this.isLoading = false;
         this.dataChanges.emit();
-      },(error)=>{
-        this.isLoading = false
-        console.log(error)
-      })
+      }, (error) => {
+        this.isLoading = false;
+        console.log(error);
+      });
   }
-  updateCart(){
-    this.isLoading = true
+  updateCart() {
+    this.isLoading = true;
     this.productService.updateCart(this.displayData.cartId, this.cartForm).subscribe(
-      (res)=>{
-        this.isLoading = false
+      (res) => {
+        this.isLoading = false;
         this.dataChanges.emit();
-      },(error)=>{
-        this.isLoading = false
-        console.log(error)
+      }, (error) => {
+        this.isLoading = false;
+        console.log(error);
       }
-    )
+    );
   }
   // get the products in shopping cart
-  getCartProds(){
+  getCartProds() {
     this.productService.getCardProd(this.displayData.cartId).subscribe(
-      (res)=>{
-        this.cartProdList = res
-      },(error)=>{
-        console.log(error)
+      (res) => {
+        this.cartProdList = res;
+      }, (error) => {
+        console.log(error);
       }
-    )
+    );
   }
-  deleteCardProd(id){
+  deleteCardProd(id) {
     this.productService.deleteCartProd(id).subscribe(
-      (res)=>{
-        this.getCartProds()
-    }, (error)=>{
-      console.log(error)
-    })
+      (res) => {
+        this.getCartProds();
+    }, (error) => {
+      console.log(error);
+    });
   }
 
 
