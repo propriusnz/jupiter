3663d42f95ba6c1b80773e321688ed25d7221756
lgxkevin <lgx9587@gmail.com>
2019-04-30 21:41:59 +1200

    update shopping cart check

--- src/app/fcomponents/shopping_cart/shopping_cart.component.ts
+++ src/app/fcomponents/shopping_cart/shopping_cart.component.ts
@@ -12,6 +12,7 @@ export class Shopping_cartComponent implements OnInit {
   productDetail: any;
   stockUnavailable = false;
   userInputQuantityArray = [];
+  errors = 0;
   @ViewChild('quantityInput') quantityInput: ElementRef;
   constructor(
     private productService: ProductService
@@ -53,16 +54,29 @@ export class Shopping_cartComponent implements OnInit {
   }
 
   quantityChanged(e, i) {
+    let errorAmount = 0;
     this.userInputQuantityArray[i] = e.srcElement.valueAsNumber;
     let unitPrice = this.prodsInCart[i].Price / this.prodsInCart[i].Quantity;
     let totalPriceOfProduct = unitPrice * this.userInputQuantityArray[i];
     // shopping cart in valid
     if (this.prodsInCart[i].availableStock >= this.userInputQuantityArray[i]) {
-      this.productService.setShoppingCartStatus(true);
+      // this.productService.setShoppingCartStatus(true);
       this.prodsInCart[i].Quantity = this.userInputQuantityArray[i];
       this.prodsInCart[i].Price = totalPriceOfProduct;
       this.price();
       localStorage.setItem('cartList', JSON.stringify(this.prodsInCart));
+    } else {
+      // this.productService.setShoppingCartStatus(false);
+    }
+    for (let a = 0; a < this.userInputQuantityArray.length; a++) {
+      if (this.userInputQuantityArray[a] > this.prodsInCart[a].availableStock) {
+        errorAmount += 0;
+      } else {
+        errorAmount += 1;
+      }
+    }
+    if (errorAmount === 3) {
+      this.productService.setShoppingCartStatus(true);
     } else {
       this.productService.setShoppingCartStatus(false);
     }
