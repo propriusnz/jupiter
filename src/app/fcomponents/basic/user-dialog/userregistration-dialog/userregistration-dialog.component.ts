63afb2dcab19fbece3b88990419435303af6feda
Zhuohao Li <lizhuohao@Zhuohaos-MBP.fritz.box>
2019-12-04 15:21:07 +1300

    fixed user-registration validators

--- src/app/fcomponents/basic/user-dialog/userregistration-dialog/userregistration-dialog.component.ts
+++ src/app/fcomponents/basic/user-dialog/userregistration-dialog/userregistration-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import {FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { isPlatformServer } from '@angular/common';
 @Component({
   selector: 'app-userregistration-dialog',
   templateUrl: './userregistration-dialog.component.html',
@@ -8,24 +9,53 @@ import {FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 export class UserregistrationDialogComponent implements OnInit {
   registrationForm:FormGroup;
   hide=true;
-  nameErrorMessage="Please enter valid username"
+  nameErrorMessage="Please enter valid username";
+  passwordvalue="adsfadf";
   constructor(private fb:FormBuilder){}
   ngOnInit() {
     this.registrationForm=this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100),Validators.email]],
-      password: ['',Validators.required],
-      passwordagain:['',Validators.required],
-    })
-    console.log(this.registrationForm);
+      username: ['', [Validators.required, Validators.email,Validators.minLength(8)]],
+      password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]],
+      confirmpassword:['',[Validators.required]],
+    },{validator:this.MustMatch('password','confirmpassword')
+  }); 
     
   }
   getErrorMessage() {
-    return this.username.hasError('required') ? 'You must enter a value' :
-        this.username.hasError('email') ? 'Not a valid username' :
-            '';
+    return this.username.hasError('required') ? 'Please enter a value' :
+        this.username.hasError('email') ? 'Not a valid username' : '' }
+  getErrorMessage2(){
+    return this.password.hasError('required') ? 'Please enter a value' :
+    this.password.hasError('minlength') ? 'Please enter at least 8 characters' :
+    this.password.hasError('maxlength') ? 'Please enter no more than 20 characters' :''
+  }
+  getErrorMessage3(){
+    return this.confirmpassword.hasError('required') ? 'Please enter a value': this.confirmpassword.hasError('notSame') ? 'Password does not match' : ''
+  }
+  MustMatch(controlName:string,matchingControlName:string){
+    return(group:FormGroup)=>{
+      const control=group.controls[controlName];
+      const matchingControl=group.controls[matchingControlName];
+      if(matchingControl.errors && !matchingControl.errors.notSame){
+        //return if another validator has already found an error on the matchingControl
+        return;
+      }
+      //set error on matchingControl if validation fails
+      if(control.value!==matchingControl.value){
+        matchingControl.setErrors({notSame:true});
+      }else{
+        matchingControl.setErrors(null);
+      }
+    
+      
+    };
+
   }
   get username(){return this.registrationForm.get('username')};
   get password(){return this.registrationForm.get('password')};
-  get passwordagain(){return this.registrationForm.get('passwordagain')};
-    
+  get confirmpassword(){return this.registrationForm.get('confirmpassword')};
+  onSubmit(value){
+    console.log(value.password,value.confirmpassword);
+  }
 }
+
