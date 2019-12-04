cde1ade1fa17a0d4533c0ec164e6004ab8ba05fd
Zhuohao Li <lizhuohao@Zhuohaos-MBP.fritz.box>
2019-12-04 18:14:34 +1300

    Added pattern for setting up password

--- src/app/fcomponents/basic/user-dialog/userregistration-dialog/userregistration-dialog.component.ts
+++ src/app/fcomponents/basic/user-dialog/userregistration-dialog/userregistration-dialog.component.ts
@@ -15,7 +15,7 @@ export class UserregistrationDialogComponent implements OnInit {
   ngOnInit() {
     this.registrationForm=this.fb.group({
       username: ['', [Validators.required, Validators.email,Validators.minLength(8)]],
-      password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]],
+      password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20),Validators.pattern('(?!^[0-9 ]*$)(?!^[a-zA-Z ]*$)^([a-zA-Z0-9 ]{8,20})$')]],
       confirmpassword:['',[Validators.required]],
     },{validator:this.MustMatch('password','confirmpassword')
   }); 
@@ -27,7 +27,8 @@ export class UserregistrationDialogComponent implements OnInit {
   getErrorMessage2(){
     return this.password.hasError('required') ? 'Please enter a value' :
     this.password.hasError('minlength') ? 'Please enter at least 8 characters' :
-    this.password.hasError('maxlength') ? 'Please enter no more than 20 characters' :''
+    this.password.hasError('maxlength') ? 'Please enter no more than 20 characters' :
+    this.password.hasError('pattern') ? 'Please use combination of letters and characters' :''
   }
   getErrorMessage3(){
     return this.confirmpassword.hasError('required') ? 'Please enter a value': this.confirmpassword.hasError('notSame') ? 'Password does not match' : ''
