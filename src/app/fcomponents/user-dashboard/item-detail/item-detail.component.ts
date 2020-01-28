27977e74684dbead8775247ab641442e4759a707
eric <eric.chong8536@gmail.com>
2020-01-28 15:04:44 +1300

    update3 akira

--- src/app/fcomponents/user-dashboard/item-detail/item-detail.component.ts
+++ src/app/fcomponents/user-dashboard/item-detail/item-detail.component.ts
@@ -11,39 +11,12 @@ export class ItemDetailComponent implements OnInit {
   
   items: any
   orderID: any
-  // id: number
   constructor(
     public productService: ProductService,
     @Inject(MAT_DIALOG_DATA) data,
   ) { 
     console.log(data)
     this.orderID = data
-
-    this.items = [{
-      item: "Corsage",
-      quantity: "2",
-      price: "$0.00",
-      id: 200
-  },
-  {
-      item: "White Roman Column Pillar&White Tall Stone Look Urns",
-      quantity: "3",
-      price:"$20.00",
-      id: 237
-  },
-  {
-      item: "Wedding Chair set",
-      quantity: "10",
-      price:"$148",
-      id: 177
-  },
-  {
-      item: "Party Package 2",
-      quantity: "1",
-      price:"$349",
-      id: 148
-  }]
-  // this.id = 200;
   }
 
   ngOnInit() {
--- src/app/fcomponents/user-dashboard/order-history/order-history.component.ts
+++ src/app/fcomponents/user-dashboard/order-history/order-history.component.ts
@@ -11,56 +11,19 @@ import { ProductService } from 'src/app/service/product.service';
 export class OrderHistoryComponent implements OnInit {
   orders: any
   userID = 2
-  // 这userid是向后端取的,也是product service那里带过来，这userid为 2
   constructor( 
     public dialog: MatDialog,
     public productService: ProductService
     ) {
-  //   this.orders = [{
-  //     eventAddress: "19 William Roberts Road, Pakuranga, Auckland 2010",
-  //     dateOfEvent: "08 January 2020",
-  //     items : 1,
-  //     price: "$1889.00",
-  //     paymentStatus: "Paid",
-  //     orderStatus: "Collected"
-  // },
-  // {
-  //     eventAddress: "8 Cortina Place, Pakuranga, Auckland 2010",
-  //     dateOfEvent: "10 January 2020",
-  //     items : 2,
-  //     price: "$1889.00",
-  //     paymentStatus: "Unpaid",
-  //     orderStatus: "Not Collected"
-  // },
-  // {
-  //     eventAddress: "25 Kilkenny Drive, Dannemora, Auckland 2016",
-  //     dateOfEvent: "12 January 2020",
-  //     items : "Order 3",
-  //     price: "$1889.00",
-  //     paymentStatus: "Expired",
-  //     orderStatus: "Collected"
-  // },
-  // {
-  //     eventAddress: "36-46 Lagoon Drive, Panmure, Auckland 1072",
-  //     dateOfEvent: "15 January 2020",
-  //     items : "Order 4",
-  //     price: "$1889.00",
-  //     paymentStatus: "Paid",
-  //     orderStatus: "Returned"
-  // }]
  }
 
   ngOnInit() {
     this.productService.getUserOrdersHistory(this.userID).subscribe(
-      // 从productservice哪里找getUserOrdersHistory，然后找userid, 意思是2，然后订阅
       (res)=>{console.log(res),
         this.orders = res
-        //res是response,是向后端请求获取的资料,同一时间也要看后端的反应是什么
       },
       (err)=>{console.warn(err)}
-      // 如果请求失败，那就会显示error
     )
-
   }
 
   itemDialog(id) {
