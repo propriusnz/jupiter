41e5614d463f4235a64e8d98084687fbd53c22bf
mikel23@hotmail.co.nz <mikel23@hotmail.co.nz>
2020-03-19 12:59:36 +1300

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   // baseUrl: 'http://192.168.178.96:5000/api',
 
   // baseUrl: 'http://localhost:5000/Api',
-   baseLink: 'https://storage.cloud.google.com/luxe_media/wwwroot/',
+   baseLink: 'https://storage.googleapis.com/luxe_media/wwwroot/',
   // baseLink: 'http://localhost:5000/'
   //baseLink: 'http://45.76.123.59:5001/',
   //  baseUrl: 'http://45.76.123.59:5001/api',
